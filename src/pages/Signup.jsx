5c8172d017eaba82495d3d5221bfecc0ace2bce0
vectinox <vectinox.agency@gmail.com>
2024-12-07 14:59:46 +0500

    login-signup update

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,40 +2,41 @@ import React from 'react';
 
 const Signup = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold text-center text-gray-700">Sign Up</h2>
-        <form className="mt-4">
+    <div className="flex items-center justify-center h-screen bg-gray-800 p-8 lg:p-0">
+      <div className="w-full max-w-md p-6 bg-gray-600 rounded-lg shadow-md">
+      <h1 className='text-6xl font-bold text-center text-orange-400 mb-8 border-b-2'>PADHLAY</h1>
+        <h2 className="text-3xl font-bold text-center text-white">Sign Up</h2>
+        <form className="mt-2">
           <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-600">Name</label>
+            <label className="block text-md font-medium text-white">Name</label>
             <input
               type="text"
               placeholder="Enter your name"
-              className="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-600"
+              className="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-700"
             />
           </div>
           <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-600">Email</label>
+            <label className="block text-md font-medium text-white">Email</label>
             <input
               type="email"
               placeholder="Enter your email"
-              className="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-600"
+              className="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-700"
             />
           </div>
           <div className="mb-4">
-            <label className="block text-sm font-medium text-gray-600">Password</label>
+            <label className="block text-sm font-medium text-white">Password</label>
             <input
               type="password"
               placeholder="Enter your password"
-              className="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-600"
+              className="w-full px-4 py-2 mt-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-700"
             />
           </div>
-          <button className="w-full px-4 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-500">
+          <button className="w-full font-semibold px-4 py-2 text-white bg-orange-600 rounded-lg hover:bg-orange-700">
             Sign Up
           </button>
         </form>
-        <p className="mt-4 text-sm text-center text-gray-600">
-          Already have an account? <a href="/login" className="text-blue-600">Login</a>
+        <p className="mt-4 text-md text-center text-white">
+          Already have an account? <a href="/login" className="text-orange-500 font-bold">Login</a>
         </p>
       </div>
     </div>
